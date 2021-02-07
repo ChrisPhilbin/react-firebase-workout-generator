96b5dc2e35ce78398dc0703409ae3eb1c7272fef
Chris Philbin <chris@chrisphilbin.net>
2021-02-07 09:20:09 -0500

    Setting up the reat router and implementing place holder component for the login component

--- view/src/App.js
+++ view/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import login from './pages/login'
 
 const App = () => {
   return(
-    <div>
-      Hello from React!
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/login" component={login}/>
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
---
+++ view/src/pages/login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const login = () => {
+    return(
+        <div>
+            Hello from login component
+        </div>
+    )
+}
+
+export default login
