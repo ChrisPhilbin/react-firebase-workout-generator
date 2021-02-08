c48caab0adcf11fd28873c39142327f6d9772cfe
Chris Philbin <chris@chrisphilbin.net>
2021-02-08 18:49:08 -0500

    Adding material-ui components to the DOM and updating Login component name to begin with an upper case letter

--- view/src/App.js
+++ view/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import login from './pages/login'
+import Login from './pages/Login'
 
 const App = () => {
   return(
     <Router>
       <div>
         <Switch>
-          <Route exact path="/login" component={login}/>
+          <Route exact path="/login" component={Login}/>
         </Switch>
       </div>
     </Router>
