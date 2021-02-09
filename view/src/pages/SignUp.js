60743939a61ff80a3d71fc429b1c5cadc25a73a3
Chris Philbin <chris@chrisphilbin.net>
2021-02-08 22:33:44 -0500

    Implementing material-ui components to render to the DOM

--- view/src/pages/SignUp.js
+++ view/src/pages/SignUp.js
@@ -79,9 +79,165 @@ const SignUp = (props) => {
     const { classes } = props
 
     return(
-        <div>
-            Hello
-        </div>
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Sign up
+                </Typography>
+                <form className={classes.form} noValidate>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="firstName"
+                                label="First Name"
+                                name="firstName"
+                                autoComplete="firstName"
+                                helperText={errors.firstName}
+                                error={errors.firstName ? true : false}
+                                onChange={e => setFirstName(e.target.value)}
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="lastName"
+                                label="Last Name"
+                                name="lastName"
+                                autoComplete="lastName"
+                                helperText={errors.lastName}
+                                error={errors.lastName ? true : false}
+                                onChange={e => setLastName(e.target.value)}
+                            />
+                        </Grid>
+
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="username"
+                                label="User Name"
+                                name="username"
+                                autoComplete="username"
+                                helperText={errors.username}
+                                error={errors.username ? true : false}
+                                onChange={e => setUsername(e.target.value)}
+                            />
+                        </Grid>
+
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="phoneNumber"
+                                label="Phone Number"
+                                name="phoneNumber"
+                                autoComplete="phoneNumber"
+                                pattern="[7-9]{1}[0-9]{9}"
+                                helperText={errors.phoneNumber}
+                                error={errors.phoneNumber ? true : false}
+                                onChange={e => setPhoneNumber(e.target.value)}
+                            />
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                autoComplete="email"
+                                helperText={errors.email}
+                                error={errors.email ? true : false}
+                                onChange={e => setEmail(e.target.value)}
+                            />
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="country"
+                                label="Country"
+                                name="country"
+                                autoComplete="country"
+                                helperText={errors.country}
+                                error={errors.country ? true : false}
+                                onChange={e => setCountry(e.target.value)}
+                            />
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                                autoComplete="current-password"
+                                helperText={errors.password}
+                                error={errors.password ? true : false}
+                                onChange={e => setPassword(e.target.value)}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                name="confirmPassword"
+                                label="Confirm Password"
+                                type="password"
+                                id="confirmPassword"
+                                autoComplete="current-password"
+                                onChange={e => setConfirmPassword(e.target.value)}
+                            />
+                        </Grid>
+                    </Grid>
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                        onClick={this.handleSubmit}
+                        disabled={loading || 
+                            !this.state.email || 
+                            !this.state.password ||
+                            !this.state.firstName || 
+                            !this.state.lastName ||
+                            !this.state.country || 
+                            !this.state.username || 
+                            !this.state.phoneNumber}
+                    >
+                        Sign Up
+                        {loading && <CircularProgress size={30} className={classes.progess} />}
+                    </Button>
+                    <Grid container justify="flex-end">
+                        <Grid item>
+                            <Link href="login" variant="body2">
+                                Already have an account? Sign in
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </form>
+            </div>
+        </Container>
     )
 }
 
