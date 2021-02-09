b8eadf4b2e2bcd6ebfff9089d73a0d354abea1eb
Chris Philbin <chris@chrisphilbin.net>
2021-02-08 19:28:47 -0500

    Implementing basic SignUp component and setting up the imports from material-ui

--- view/src/App.js
+++ view/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Login from './pages/Login'
+import SignUp from './pages/SignUp'
 
 const App = () => {
   return(
     <Router>
       <div>
         <Switch>
-          <Route exact path="/login" component={Login}/>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={SignUp} />
         </Switch>
       </div>
     </Router>
