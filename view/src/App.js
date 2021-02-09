e9aa440fdac465a3ba8a17d22c6b34d65e83d101
Chris Philbin <chris@chrisphilbin.net>
2021-02-09 07:10:44 -0500

    Creating basic Home component and importing it into App and assigning it the root route

--- view/src/App.js
+++ view/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Login from './pages/Login'
+import Home   from './pages/Home'
+import Login  from './pages/Login'
 import SignUp from './pages/SignUp'
 
 const App = () => {
@@ -8,7 +9,8 @@ const App = () => {
     <Router>
       <div>
         <Switch>
-          <Route exact path="/login" component={Login} />
+          <Route exact path="/"       component={Home}   />
+          <Route exact path="/login"  component={Login}  />
           <Route exact path="/signup" component={SignUp} />
         </Switch>
       </div>
---
+++ view/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+
+const Home = (props) => {
+    return(
+        <div>
+            Welcome home!
+        </div>
+    )
+}
+
+export default Home
