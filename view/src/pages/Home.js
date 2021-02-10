18427c6266589a8e2c88bb1f2b4d4203a6dcfd3c
Chris Philbin <chris@chrisphilbin.net>
2021-02-09 20:56:13 -0500

    Adding imports from material-ui, defining styles and creating functions to update render variable when user clicks links

--- view/src/pages/Home.js
+++ view/src/pages/Home.js
@@ -1,20 +1,76 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import withStyles from '@material-ui/core/styles/withStyles';
+import AccountBoxIcon from '@material-ui/icons/AccountBox';
+import NotesIcon from '@material-ui/icons/Notes';
+import Avatar from '@material-ui/core/Avatar';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
 import Account from '../components/Account'
 import Exercise from '../components/Exercise'
 
+const drawerWidth = 240;
+
+const styles = (theme) => ({
+	root: {
+		display: 'flex'
+	},
+	appBar: {
+		zIndex: theme.zIndex.drawer + 1
+	},
+	drawer: {
+		width: drawerWidth,
+		flexShrink: 0
+	},
+	drawerPaper: {
+		width: drawerWidth
+	},
+	content: {
+		flexGrow: 1,
+		padding: theme.spacing(3)
+	},
+	avatar: {
+		height: 110,
+		width: 100,
+		flexShrink: 0,
+		flexGrow: 0,
+		marginTop: 20
+	},
+	uiProgess: {
+		position: 'fixed',
+		zIndex: '1000',
+		height: '31px',
+		width: '31px',
+		left: '50%',
+		top: '35%'
+	},
+	toolbar: theme.mixins.toolbar
+})
+
 const Home = (props) => {
 
-    let [firstName, setFirstName] = useState('')
-    let [lastName, setLastName]   = useState('')
-    let [email, setEmail]         = useState('')
-    let [phoneNumber, setPhoneNumber] = useState('')
-    let [country, setCountry]         = useState('')
-    let [userName, setUserName]       = useState('')
-    let [uiLoading, setUiLoading]     = useState(false)
+    let [render, setRender]                 = useState(false)
+    let [firstName, setFirstName]           = useState('')
+    let [lastName, setLastName]             = useState('')
+    let [email, setEmail]                   = useState('')
+    let [phoneNumber, setPhoneNumber]       = useState('')
+    let [country, setCountry]               = useState('')
+    let [userName, setUserName]             = useState('')
+    let [uiLoading, setUiLoading]           = useState(false)
     let [profilePicture, setProfilePicture] = useState('')
-    let [errors, setErrors] = useState({})
+    let [errors, setErrors]                 = useState({})
 
     useEffect(() => {
         authMiddleWare(props.history);
@@ -40,11 +96,24 @@ const Home = (props) => {
 			});
     }, [])
 
+    const loadAccountPage = (event) => {
+        setRender(true)
+	}
+
+	const loadTodoPage = (event) => {
+        setRender(false)
+	}
+
+	const logoutHandler = (event) => {
+		localStorage.removeItem('AuthToken');
+		props.history.push('/login');
+	};
+
     return(
         <div>
-            Welcome home!
+            Home
         </div>
     )
 }
 
-export default Home
+export default withStyles(styles)(Home)
