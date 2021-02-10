aceaa2fca2675f7cf5c836a25e520a8db874da91
Chris Philbin <chris@chrisphilbin.net>
2021-02-10 07:19:55 -0500

    Setting up local state of component and implementing useEffect hook in order to fetch list of exercises the logged in user has created

--- view/src/components/Exercise.js
+++ view/src/components/Exercise.js
@@ -1,6 +1,10 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import withStyles from '@material-ui/core/styles/withStyles';
+
+import axios from 'axios'
+
+import authMiddleWare from '../util/Auth'
 
 const styles = ((theme) => ({
     toolbar: theme.mixins.toolbar
@@ -8,6 +12,31 @@ const styles = ((theme) => ({
 )
 
 const Exercise = (props) => {
+
+    let [exercises, setExercises]     = useState('')
+    let [name, setName]               = useState('')
+    let [muscleGroup, setMuscleGroup] = useState('')
+    let [exerciseId, setExerciseId]   = useState('')
+    let [errors, setErrors]           = useState([])
+    let [open, setOpen]               = useState('')
+    let [uiLoading, setUiLoading]     = useState(true)
+    let [buttonType, setButtonType]   = useState('')
+    let [viewOpen, setViewOpen]       = useState(false)
+
+    useEffect(() => {
+        authMiddleWare(props.history);
+		const authToken = localStorage.getItem('AuthToken');
+		axios.defaults.headers.common = { Authorization: `${authToken}` };
+		axios
+			.get('/exercises')
+			.then((response) => {
+                setExercises(response.data)
+                setUiLoading(false)
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+    }, [])
 
     const { classes } = props
 
