a3f27a69d1c8a2a106088fb32dac969d342775aa
Chris Philbin <chris@chrisphilbin.net>
2021-02-09 20:04:38 -0500

    Implementing useEffect hook to fetch user information from the API

--- view/src/pages/Home.js
+++ view/src/pages/Home.js
@@ -1,9 +1,45 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 import Account from '../components/Account'
 import Exercise from '../components/Exercise'
 
 const Home = (props) => {
+
+    let [firstName, setFirstName] = useState('')
+    let [lastName, setLastName]   = useState('')
+    let [email, setEmail]         = useState('')
+    let [phoneNumber, setPhoneNumber] = useState('')
+    let [country, setCountry]         = useState('')
+    let [userName, setUserName]       = useState('')
+    let [uiLoading, setUiLoading]     = useState(false)
+    let [profilePicture, setProfilePicture] = useState('')
+    let [errors, setErrors] = useState({})
+
+    useEffect(() => {
+        authMiddleWare(props.history);
+		const authToken = localStorage.getItem('AuthToken');
+		axios.defaults.headers.common = { Authorization: `${authToken}` };
+		axios
+			.get('/user')
+			.then((response) => {
+                setFirstName(response.data.userCredentials.firstName),
+                setLastName(response.data.userCredentials.lastName),
+                setEmail(response.data.userCredentials.email),
+                setPhoneNumber(response.data.userCredentials.phoneNumber),
+                setCountry(response.data.userCredentials.country),
+                setUserName(response.data.userCredentials.username),
+                setUiLoading(false),
+                setProfilePicture(response.data.userCredentials.imageUrl)
+			})
+			.catch((error) => {
+				if(error.response.status === 403) {
+					props.history.push('/login')
+				}
+				setErrors({ errorMsg: 'Error in retrieving the data' });
+			});
+    }, [])
+
     return(
         <div>
             Welcome home!
