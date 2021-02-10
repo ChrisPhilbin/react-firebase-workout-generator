7de58c4e1c087d3f1b83dcf36fbfae9af55c81a9
Chris Philbin <chris@chrisphilbin.net>
2021-02-10 06:56:59 -0500

    Adjusting layout to properly display content from both Exercise and Workout components - Needed to account for the height of the AppBar element that was previously hiding the displayed placeholder text

--- view/src/components/Exercise.js
+++ view/src/components/Exercise.js
@@ -1,11 +1,28 @@
 import React, { useState } from 'react'
 
+import withStyles from '@material-ui/core/styles/withStyles';
+
+const styles = ((theme) => ({
+    toolbar: theme.mixins.toolbar
+    })
+)
+
 const Exercise = (props) => {
+
+    const { classes } = props
+
     return(
-        <div>
-            Exercise component
-        </div>
+        <>
+            <div className={classes.toolbar} />
+
+            <div>
+                <h1>Exercise component</h1><br />
+                <h1>Exercise component</h1><br />
+                <h1>Exercise component</h1><br />
+                <h1>Exercise component</h1><br />
+            </div>
+        </>
     )
 }
 
-export default Exercise
+export default withStyles(styles)(Exercise)
--- view/src/components/Workout.js
+++ view/src/components/Workout.js
@@ -1,6 +1,16 @@
 import React from 'react'
 
+import withStyles from '@material-ui/core/styles/withStyles';
+
+const styles = ((theme) => ({
+    toolbar: theme.mixins.toolbar
+    })
+)
+
 const Workout = (props) => {
+
+    const { classes } = props
+
     return(
         <div>
             Workout component
@@ -8,4 +18,4 @@ const Workout = (props) => {
     )
 }
 
-export default Workout
+export default withStyles(styles)(Workout)
