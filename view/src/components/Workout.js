60746877728618ef32d7c3b5712da8b283b60374
Chris Philbin <chris@chrisphilbin.net>
2021-02-12 07:13:34 -0500

    Implementing slider element from material-ui in order to allow the user to select a range of exercises they'd like to use as a random number when generating their workout

--- view/src/components/Workout.js
+++ view/src/components/Workout.js
@@ -1,12 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+import Typography from '@material-ui/core/Typography';
+import Slider from '@material-ui/core/Slider'
+import withStyles from '@material-ui/core/styles/withStyles';
+
+const styles = (theme) => ({
+
+    toolbar: theme.mixins.toolbar,
+    root: {
+        width: 300
+    }
+
+})
 
 const Workout = (props) => {
 
-    return(
-        <div>
-            Workout component
-        </div>
+    const { classes } = props
+
+    const valuetext = (value) => {
+        return `Between ${value[0]} and ${value[1]} number of exercises`
+      }      
+
+    const [value, setValue] = useState([8, 15])
+
+    const handleChange = (event, newValue) => {
+      setValue(newValue);
+    };
+  
+    return (
+    <>
+        {console.log(value)}
+      <div className={classes.toolbar} />
+      <div className={classes.root}>
+        <Typography id="range-slider" gutterBottom>
+          Select range of exercises
+        </Typography>
+        <Slider
+          value={value}
+          min={0}
+          max={25}
+          onChange={handleChange}
+          valueLabelDisplay="auto"
+          aria-labelledby="range-slider"
+          getAriaValueText={valuetext}
+        />
+      </div>
+    </>
     )
 }
 
-export default Workout
+export default withStyles(styles)(Workout)
