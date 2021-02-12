07912000bb1763ce1c8ec8dbdfa2e869c797460e
Chris Philbin <chris@chrisphilbin.net>
2021-02-12 15:24:20 -0500

    Adding slider element from material-ui for selecting desired range of reps when generating a random workout

--- view/src/components/Workout.js
+++ view/src/components/Workout.js
@@ -15,34 +15,58 @@ const styles = (theme) => ({
 
 const Workout = (props) => {
 
+    const [exerciseRange, setExerciseRange] = useState([8, 15])
+    const [repRange, setRepRange]           = useState([8, 12])
+
     const { classes } = props
 
-    const valuetext = (value) => {
-        return `Between ${value[0]} and ${value[1]} number of exercises`
+    const exercisetext = (value) => {
+        return `Between ${exerciseRange[0]} and ${exerciseRange[1]} number of exercises`
       }      
 
-    const [value, setValue] = useState([8, 15])
-
-    const handleChange = (event, newValue) => {
-      setValue(newValue);
+    const handleExerciseChange = (event, newValue) => {
+      setExerciseRange(newValue);
     };
+
+    const reptext = (value) => {
+      return `Between ${repRange[0]} and ${repRange[1]} number of reps`
+    }      
+
+    const handleRepChange = (event, newValue) => {
+      setRepRange(newValue);
+    };
+
   
     return (
     <>
-        {console.log(value)}
+    {console.log(exerciseRange)}
       <div className={classes.toolbar} />
+
       <div className={classes.root}>
         <Typography id="range-slider" gutterBottom>
           Select range of exercises
         </Typography>
         <Slider
-          value={value}
-          min={0}
+          value={exerciseRange}
+          min={1}
           max={25}
-          onChange={handleChange}
+          onChange={handleExerciseChange}
           valueLabelDisplay="auto"
           aria-labelledby="range-slider"
-          getAriaValueText={valuetext}
+          getAriaValueText={exercisetext}
+        />
+
+        <Typography id="range-slider" gutterBottom>
+          Select range of reps
+        </Typography>
+        <Slider
+          value={repRange}
+          min={5}
+          max={50}
+          onChange={handleRepChange}
+          valueLabelDisplay="auto"
+          aria-labelledby="range-slider"
+          getAriaValueText={reptext}
         />
       </div>
     </>
