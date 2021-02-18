195671a486d5e81682aa29dd634fe37fcbe1bd92
Chris Philbin <chris@chrisphilbin.net>
2021-02-18 06:14:44 -0500

    Creating placeholder for PreviousWorkoutList component that will display a list of all saved workouts that were randomly generated for the user

---
+++ view/src/components/PreviousWorkoutList.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react'
+
+const PreviousWorkoutList = (props) => {
+    return(
+        <div>
+            Previous Workout List Component
+        </div>
+    )
+}
+
+export default PreviousWorkoutList
