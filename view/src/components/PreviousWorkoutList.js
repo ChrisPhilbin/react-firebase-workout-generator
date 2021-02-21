8c000c3dedb4e3e75cf13d84001e84e54f0ae5d7
Chris Philbin <chris@chrisphilbin.net>
2021-02-21 08:17:08 -0500

    Adding momentjs and fixing issue with layout within PreviousWorkoutList component

--- view/src/components/PreviousWorkoutList.js
+++ view/src/components/PreviousWorkoutList.js
@@ -1,8 +1,17 @@
 import React, { useEffect, useState } from 'react'
+import withStyles from '@material-ui/core/styles/withStyles';
 import { authMiddleWare } from '../util/Auth'
+import moment from 'moment'
 import axios from 'axios'
 
+const styles = ((theme) => ({
+  toolbar: theme.mixins.toolbar,
+  })
+)
+
 const PreviousWorkoutList = (props) => {
+
+    let { classes } = props
 
     let [previousWorkous, setPreviousWorkouts] = useState([])
 
@@ -23,11 +32,15 @@ const PreviousWorkoutList = (props) => {
       }, []);
 
     return(
+      <>
+        <div className={classes.toolbar} />
+
         <div>
             {console.log(previousWorkous, "Previous workouts")}
             Previous Workout List Component
         </div>
+      </>
     )
 }
 
-export default PreviousWorkoutList
+export default withStyles(styles)(PreviousWorkoutList)
